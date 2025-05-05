6ad1bdea94e2469c45353fbd909e0b46b58bee61
Valeria D'Amante <valeriadamante@gmail.com>
2025-05-05 15:24:24 +0200

    updated env

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-# apply_cmssw_customization_steps() {
+apply_cmssw_customization_steps() {
 #     run_cmd mkdir -p HHTools
 #     run_cmd ln -s "$this_dir/HHbtag" HHTools/HHbtag
 #     run_cmd mkdir -p TauAnalysis
@@ -6,7 +6,9 @@
 #     run_cmd ln -s "$this_dir/SVfitTF" TauAnalysis/SVfitTF
 #     run_cmd mkdir -p HHKinFit2
 #     run_cmd ln -s "$this_dir/HHKinFit2" HHKinFit2/HHKinFit2
-# }
+    run_cmd git cms-init
+    run_cmd git cms-addpkg Validation/MuonCSCDigis
+}
 
 action() {
     local this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
@@ -14,8 +16,6 @@ action() {
     local this_file_path="$this_dir/$(basename $this_file)"
 
     export ANALYSIS_PATH="$this_dir"
-    # export HH_INFERENCE_PATH="$ANALYSIS_PATH/inference"
-
     source $this_dir/GEM-CSC-trg-dev/env.sh "$this_file_path" "$@"
 }
 
